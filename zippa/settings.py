b8b4d3315402da98b785130cedbca0c6102c57b5
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-07-04 15:45:55 +0000

    Updated the negotiations view to enable rider and customer negotiate on a price before deal

--- zippa/settings.py
+++ zippa/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://zippa-backend-z90m.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
